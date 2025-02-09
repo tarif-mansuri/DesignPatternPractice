4597da0e5bbc0088cbc866d7b4a6c265f8cfbe32
tarif.m <tarif.m@zohocorp.com>
2025-02-09 18:27:30 +0530

    Solution has been added

--- src/com/assignment/question/FileBasedConfigurationManagerImpl.java
+++ src/com/assignment/question/FileBasedConfigurationManagerImpl.java
@@ -1,49 +1,58 @@
 package com.assignment.question;
 
 public class FileBasedConfigurationManagerImpl extends FileBasedConfigurationManager{
+    private static FileBasedConfigurationManagerImpl instance;
+
+    private FileBasedConfigurationManagerImpl(){
+        super();
+    }
 
     @Override
     public String getConfiguration(String key) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+        return instance.properties.getProperty(key);
     }
 
     @Override
     public <T> T getConfiguration(String key, Class<T> type) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+        String val = getConfiguration(key);
+        if(val!=null){
+            return instance.convert(val, type);
+        }
+        return null;
     }
 
     @Override
     public void setConfiguration(String key, String value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+        instance.properties.put(key, value);
     }
 
     @Override
     public <T> void setConfiguration(String key, T value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+        instance.properties.put(key, value.toString());
     }
 
     @Override
     public void removeConfiguration(String key) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeConfiguration'");
+        instance.properties.remove(key);
     }
 
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+        instance.properties.clear();
     }
 
     public static FileBasedConfigurationManager getInstance() {
-        // TODO Auto-generated method stub
-        return null;
+        if(instance == null) {
+            synchronized (FileBasedConfigurationManagerImpl.class) {
+                if(instance == null) {
+                    instance = new FileBasedConfigurationManagerImpl();
+                }
+            }
+        }
+        return instance;
     }
 
     public static void resetInstance() {
-        // TODO Auto-generated method stub
+        instance = null;
     }
 }
